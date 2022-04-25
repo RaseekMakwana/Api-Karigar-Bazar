275e2a55819487a5cd6e3f73c363a37ec6a62101
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-04-26 02:22:58 +0530

    s

--- application/controllers/CategoryController.php
+++ application/controllers/CategoryController.php
@@ -5,14 +5,15 @@ class CategoryController extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		$this->common->header_authentication();
+		// $this->common->header_authentication();
 	}
 	public function get_categories_by_vendor_type()
 	{
 		$request = $this->input->post();
-		$this->common->field_required(array('vendor_type_id'),$request);
+		// $this->common->field_required(array('vendor_type_id'),$request);
 
-		$query_results = $this->db->query("select * from category_master where vendor_type_id='".$request['vendor_type_id']."'")->result();
+		$query_results = $this->db->query("select * from category_master")->result();
+		// $query_results = $this->db->query("select * from category_master where vendor_type_id='".$request['vendor_type_id']."'")->result();
 		$data = array();
 		foreach($query_results as $row){
 			$array_category = array(
