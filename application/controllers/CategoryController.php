5145b8b9e48fd1d75526799b1dd26a4fdb371e83
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-04-26 22:41:48 +0530

    s

--- application/controllers/CategoryController.php
+++ application/controllers/CategoryController.php
@@ -5,13 +5,36 @@ class CategoryController extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		$this->common->header_authentication();
+		// $this->common->header_authentication();
 	}
 	public function get_categories_by_vendor_type()
 	{
 		$request = $this->input->post();
+		// $this->common->field_required(array('vendor_type_id'),$request);
+
+		$query_results = $this->db->query("select * from category_master")->result();
+		$data = array();
+		foreach($query_results as $row){
+			$array_category = array(
+				"category_id"=>$row->category_id,
+				"category_name"=>$row->category_name,
+				"picture_thumb"=>$row->picture_thumb
+			);
+			$data[] = array_map("strval",$array_category);
+		}
+
+		$response['status'] = 1;
+		$response['message'] = DATA_GET_SUCCESSFULLY;
+		$response['data'] = $data;
+
+		$this->common->response($response);
+	}
+	public function post_get_categories_by_vendor_type()
+	{
+		$request = $this->input->post();
 		$this->common->field_required(array('vendor_type_id'),$request);
 
+		// $query_results = $this->db->query("select * from category_master")->result();
 		$query_results = $this->db->query("select * from category_master where vendor_type_id='".$request['vendor_type_id']."'")->result();
 		$data = array();
 		foreach($query_results as $row){
