0c7aaf29cfd19cd993b8f85853c9be8a3ce4e346
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-04-27 10:40:57 +0530

    s

--- application/controllers/CategoryController.php
+++ application/controllers/CategoryController.php
@@ -40,21 +40,37 @@ class CategoryController extends CI_Controller {
 	{
 		$request = $this->input->post();
 
-		$query_results = $this->db->query("SELECT vtm.vendor_type_id,vtm.`vendor_type_name`,cm.`category_id`,cm.`category_name`,scm.`sub_category_id`,scm.`sub_category_name`
-		FROM vendor_type_master AS vtm 
-		LEFT JOIN category_master AS cm ON cm.`vendor_type_id`=vtm.`vendor_type_id` AND cm.`status`='1'
-		LEFT JOIN `sub_category_master` AS scm ON scm.`category_id`=cm.`category_id` AND scm.`status`='1'
-		WHERE vtm.`status`='1'")->result();
+		$query_results = $this->db->query("SELECT vendor_type_id,vendor_type_name FROM vendor_type_master WHERE status='1'")->result();
 	// p($query_results);
 
 		$response_data = array();
+		$index1=0;
 		foreach($query_results as $row){
-			$response_data[$row->vendor_type_id]['vendor_type_id'] = $row->vendor_type_id;
-			$response_data[$row->vendor_type_id]['vendor_type_name'] = $row->vendor_type_name;
-			$response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['category_id'] = $row->category_id;
-			$response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['category_name'] = $row->category_name;
-			$response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['sub_category_data'][$row->sub_category_id]['sub_category_id'] = $row->sub_category_id;
-			$response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['sub_category_data'][$row->sub_category_id]['sub_category_name'] = $row->sub_category_name;
+			$response_data[$index1]['vendor_type_id'] = $row->vendor_type_id;
+			$response_data[$index1]['vendor_type_name'] = $row->vendor_type_name;
+
+			$index2=0;
+			$query_results1 = $this->db->query("SELECT category_id,category_name FROM category_master where vendor_type_id='".$row->vendor_type_id."' AND status='1'")->result();
+			foreach($query_results1 as $row1){
+				$response_data[$index1]['category_data'][$index2]['category_id'] = $row1->category_id;
+				$response_data[$index1]['category_data'][$index2]['category_name'] = $row1->category_name;
+
+					$index3=0;
+					$query_results2 = $this->db->query("SELECT sub_category_id,sub_category_name FROM sub_category_master where category_id='".$row1->category_id."' AND status='1'")->result();
+					foreach($query_results2 as $row2){
+						$response_data[$index1]['category_data'][$index2]['sub_category_data'][$index3]['sub_category_id'] = $row2->sub_category_id;
+						$response_data[$index1]['category_data'][$index2]['sub_category_data'][$index3]['sub_category_name'] = $row2->sub_category_name;
+						$index3++;
+					}
+					
+				$index2++;
+			}
+			// $response_data[$row->vendor_type_id]['vendor_type_name'] = $row->vendor_type_name;
+			// $response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['category_id'] = $row->category_id;
+			// $response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['category_name'] = $row->category_name;
+			// $response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['sub_category_data'][$row->sub_category_id]['sub_category_id'] = $row->sub_category_id;
+			// $response_data[$row->vendor_type_id]['categories_data'][$row->category_id]['sub_category_data'][$row->sub_category_id]['sub_category_name'] = $row->sub_category_name;
+			$index1++;
 		}
 
 		$response['status'] = 1;
