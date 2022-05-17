94ffc8fa492cde752648e09aa837a60ad9999277
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-05-17 10:19:07 +0530

    s

--- application/controllers/ExtraController.php
+++ application/controllers/ExtraController.php
@@ -35,7 +35,7 @@ class ExtraController extends CI_Controller {
 			'upload_path' => $storage_folder,
 			'file_name' => $filename,
 			'allowed_types' => 'jpg|png',
-			'max_size' => 5000,
+			'max_size' => 2048000,
 			'encrypt_name'=>TRUE
 		);
 
