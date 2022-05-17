29b4638a8260109e8ddf755dfc11d00d8e255355
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-05-17 10:39:09 +0530

    s

--- application/controllers/ExtraController.php
+++ application/controllers/ExtraController.php
@@ -27,29 +27,39 @@ class ExtraController extends CI_Controller {
 
 	public function upload_document()
 	{
-		$filename = $this->common->preg_replace_filename($_FILES["file_upload"]['name']);
 		$request = $this->input->post();
-		$storage_folder = STORAGE_CONTENT_PATH.$request['location'].'/';
-		// p($storage_folder);
-		$config = array(
-			'upload_path' => $storage_folder,
-			'file_name' => $filename,
-			'allowed_types' => 'jpg|png',
-			'max_size' => 2048000
-		);
+		$this->common->field_required(array('location'),$request);
 
-		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload('file_upload')){
-			$response['status'] = 0;
-			$response['message'] = FILE_UPLOAD_IN_ERROR;
+		if(isset($_FILES['file_upload']['name']) && !empty($_FILES['file_upload']['name'])){
+			// for($i=0; $i<count($_FILES['file_upload']['name']); $i++){
+				$filename = $this->common->preg_replace_filename($_FILES["file_upload"]['name']);
+				
+				$storage_folder = STORAGE_CONTENT_PATH.$request['location'].'/';
+				// p($storage_folder);
+				$config = array(
+					'upload_path' => $storage_folder,
+					'file_name' => $filename,
+					'allowed_types' => 'jpg|png',
+					'max_size' => 2048000
+				);
+		
+				$this->load->library('upload', $config);
+		
+				if ( ! $this->upload->do_upload('file_upload')){
+					$response['status'] = 0;
+					$response['message'] = FILE_UPLOAD_IN_ERROR;
+				} else {
+					$response['status'] = 1;
+					$response['message'] = FILE_UPLOADED_SUCCESSFULLY;
+					$response['data'] = STORAGE_CONTENT_URL.$request['location']."/".$filename;
+				}
+				
+			// }
 		} else {
-			$response['status'] = 1;
-			$response['message'] = FILE_UPLOADED_SUCCESSFULLY;
-			$response['data'] = STORAGE_CONTENT_URL.$request['location']."/".$filename;
+			$response['status'] = 0;
+			$response['message'] = "file_upload parameter is required";
 		}
 		$this->common->response($response);
 	}
-
 
 }
