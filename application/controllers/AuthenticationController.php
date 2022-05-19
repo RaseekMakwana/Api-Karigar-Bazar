fa0cc451dbf1de25fd2c513ef6e4f20997c207fb
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-05-19 10:52:56 +0530

    s

--- application/controllers/AuthenticationController.php
+++ application/controllers/AuthenticationController.php
@@ -21,7 +21,7 @@ class AuthenticationController extends CI_Controller {
 			$user_details = $this->db->query("SELECT * FROM vendor_master WHERE mobile='".$mobile_number."'")->row();
 			$data = array(
 				"user_id" => $user_details->user_id,
-				"full_name" => $user_details->full_name,
+				"vendor_name" => $user_details->vendor_name,
 				"business_name" => $user_details->business_name,
 				"mobile" => $user_details->mobile,
 				"email" => $user_details->email,
