74f7573a44b065d002f3b52283da82563c45085d
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2022-05-21 09:16:23 +0530

    s

--- application/controllers/ExtraController.php
+++ application/controllers/ExtraController.php
@@ -31,31 +31,23 @@ class ExtraController extends CI_Controller {
 		$this->common->field_required(array('location'),$request);
 
 		if(isset($_FILES['file_upload']['name']) && !empty($_FILES['file_upload']['name'])){
-			// for($i=0; $i<count($_FILES['file_upload']['name']); $i++){
-				$filename = $this->common->preg_replace_filename($_FILES["file_upload"]['name']);
-				
-				$storage_folder = STORAGE_CONTENT_PATH.$request['location'].'/';
-				// p($storage_folder);
-				$config = array(
-					'upload_path' => $storage_folder,
-					'file_name' => $filename,
-					'allowed_types' => 'jpg|png',
-					'max_size' => 2048000
-				);
-		
-				$this->load->library('upload', $config);
-				p($request['location']."/".$filename);
-				if ($this->upload->do_upload('file_upload')){
-					
-					$response['status'] = 1;
-					$response['message'] = FILE_UPLOADED_SUCCESSFULLY;
-					$response['data'] = $request['location']."/".$filename;
-				} else {
-					$response['status'] = 0;
-					$response['message'] = FILE_UPLOAD_IN_ERROR;
-				}
-				
-			// }
+			$filename = $this->common->preg_replace_filename($_FILES["file_upload"]['name']);
+			$storage_folder = STORAGE_CONTENT_PATH.$request['location'].'/';
+			$config = array(
+				'upload_path' => $storage_folder,
+				'file_name' => $filename,
+				'allowed_types' => 'jpg|png',
+				'max_size' => 2048000
+			);
+			$this->load->library('upload', $config);
+			if ($this->upload->do_upload('file_upload')){
+				$response['status'] = 1;
+				$response['message'] = FILE_UPLOADED_SUCCESSFULLY;
+				$response['data'] = $request['location']."/".$filename;
+			} else {
+				$response['status'] = 0;
+				$response['message'] = FILE_UPLOAD_IN_ERROR;
+			}
 		} else {
 			$response['status'] = 0;
 			$response['message'] = "file_upload parameter is required";
